a9b1ba694bfe9469e1375a765a95726c2a3d6aa5
berrim0r <berrim00r@gmail.com>
2017-06-21 13:24:31 +0300

    Update config.py

--- pars/config.py
+++ pars/config.py
@@ -1,5 +1,5 @@
 home_page = "http://moscow.en.cx/"
-username = "berrim0r_en"
-password = "anu11927"
+username = "*********"
+password = "*********"
 phantom_path = "/phantomjs-2.1.1-windows/bin/phantomjs"
 my_team_xpath = "/html/body/table[3]/tbody/tr[1]/td/table/tbody/tr/td[1]/div[1]/div/div/table/tbody/tr[6]/td/a"
